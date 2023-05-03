8869bb6fccf3dc9fda55309776f4bb597d605afe
gaetbout <gaetan.ansotte@hotmail.com>
2023-05-03 21:05:40 +0200

    start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-import fs from "fs";
+import { Client } from "twitter-api-sdk";
+import "dotenv/config";
+
+const client = new Client(process.env.BEARER_TOKEN!);
+
+console.log(process.env.BEARER_TOKEN);
+
+async function main() {
+  const tweet = await client.tweets.findTweetById("20");
+  if (tweet.data) {
+      console.log(tweet.data.text);
+  } else {
+    console.log("Nothing");
+  }
+}
+
+main();
