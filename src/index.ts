81dd6a634761ab160a84c40d7fa2b8400fb4ef07
gaetbout <gaetan.ansotte@hotmail.com>
2023-05-04 20:36:19 +0200

    TODO ++ clean

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const provider = new RpcProvider({ nodeUrl: `https://starknet-mainnet.g.alchemy.
 // TODO do a tweet
 // TODO remember each block inbetween multiple boots
 // TODO Stats when is it a big tx?
-// TODO Add way of store each token + transfer_selector + decimals
+// TODO Add way of store each token + transfer_selector + decimals + HIT limit
 // TODO Pagination system if a LOT of transfer
 async function main() {
   const block_number = await provider.getBlockNumber();
@@ -23,28 +23,8 @@ async function main() {
     to_block: { block_number },
     address: ETH_ADDRESS,
     keys: [transfer_selector],
-    chunk_size: 100,
+    chunk_size: 1000,
   });
-
-  response.events
-    // .filter((e) => e.keys[0] == transfer_selector)
-    .forEach((e) => {
-      const from = e.data[0];
-      const to = e.data[1];
-      const amount = num.toBigInt(e.data[2]) + num.toBigInt(e.data[3]);
-      // console.log(`From: ${from} to: ${to} amount: ${ethers.formatUnits(amount, ETH_DECIMALS)}`);
-      console.log(`${ethers.formatUnits(amount, ETH_DECIMALS)}`);
-      // console.log(`\tTransaction hash: ${ e.transaction_hash }`);
-      // console.log(`\Find it here https://starkscan.co/tx/${ e.transaction_hash }`);
-    });
-
-  const arr = response.events
-    // .filter((e) => e.keys[0] == transfer_selector)
-    .map((e) => {
-      const amount = num.toBigInt(e.data[2]) + num.toBigInt(e.data[3]);
-      return parseFloat(ethers.formatUnits(amount, ETH_DECIMALS));
-    });
-  console.log(`Max BAD FILTER: ${Math.max(...arr)}`);
 
   const max = response.events.reduce((prev, current) => {
     const amount1 = num.toBigInt(prev.data[2]) + num.toBigInt(prev.data[3]);
@@ -53,10 +33,26 @@ async function main() {
   });
 
   console.log(max);
-  
+
   const from = await getStarkNameOrAddress(max.data[0]);
   const to = await getStarkNameOrAddress(max.data[1]);
+  const amount = num.toBigInt(max.data[2]) + num.toBigInt(max.data[3]);
+
   console.log(`From: ${from} to: ${to}`);
+  console.log(`\t${ethers.formatUnits(amount, ETH_DECIMALS)}`);
+  console.log(`\Find it here https://starkscan.co/tx/${max.transaction_hash}`);
+}
+
+async function getStarkNameOrAddress(address: string) {
+  try {
+    return await provider.getStarkName(address);
+  } catch (e) {
+    // console.log(e);
+    return address;
+  }
+}
+
+async function doTweet() {
   // const tweet = await client.tweets.findTweetById("20");
   // if (tweet.data) {
   //   console.log(tweet.data.text);
@@ -64,13 +60,4 @@ async function main() {
   //   console.log("Nothing");
   // }
 }
-
-async function getStarkNameOrAddress(address: string) {
-  try {
-    return await provider.getStarkName(address);
-  } catch ( e ) {
-    return address
-  }
-}
-
 main();
