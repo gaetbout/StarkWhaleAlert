996685c0b4cae3f5aa4c469a68051f733b87ae14
gaetbout <gaetan.ansotte@hotmail.com>
2023-05-06 14:25:27 +0200

    creating models file

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import { json } from "starknet";
 import { readFileSync, writeFileSync } from "fs";
+import { Token } from "./models";
 
 function getLastBlockNumber() {
   const jsonBlock = json.parse(readFileSync("./db/block.json").toString("ascii"));
@@ -11,16 +12,6 @@ function writeLastBlockNumber(lastProcesssedBlockNumber: number) {
   jsonBlock.lastProcesssedBlockNumber = lastProcesssedBlockNumber;
   writeFileSync("./db/block.json", json.stringify(jsonBlock));
 }
-
-type Token = {
-  address: string;
-  decimals: number;
-  symbol: string;
-  selector: string;
-  threshold: number;
-  logo: string;
-  rateApiId: string;
-};
 
 const ETH = {
   address: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
@@ -34,4 +25,4 @@ const ETH = {
 
 const tokens: Token[] = [ETH];
 
-export { Token, tokens, getLastBlockNumber, writeLastBlockNumber };
+export { tokens, getLastBlockNumber, writeLastBlockNumber };
---
+++ src/models.ts
@@ -0,0 +1,12 @@
+type Token = {
+  address: string;
+  decimals: number;
+  symbol: string;
+  selector: string;
+  threshold: number;
+  logo: string;
+  rateApiId: string;
+};
+
+// TODO Do a to tweet type
+export { Token };
