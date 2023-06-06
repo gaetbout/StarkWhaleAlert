db75ccbbbb478aebcbc35e238673400b214b4b7c
gaetbout <gaetan.ansotte@hotmail.com>
2023-06-06 11:14:46 +0200

    Run formatter

--- src/logger.ts
+++ src/logger.ts
@@ -1,21 +1,23 @@
-import chalk from 'chalk';
+import chalk from "chalk";
 
 const error = chalk.red;
-const valid= chalk.green;
+const valid = chalk.green;
 const dateColor = chalk.blue;
 const timeColor = chalk.blue.bold;
 
 export function log(msg: string, tabs = 1) {
   const date = new Date();
-  console.log(`${"\t".repeat(tabs)}${dateColor(date.toLocaleDateString())} ${timeColor(date.toLocaleTimeString("fr-FR"))} ${valid(msg)}`);
+  console.log(
+    `${"\t".repeat(tabs)}${dateColor(date.toLocaleDateString())} ${timeColor(date.toLocaleTimeString("fr-FR"))} ${valid(
+      msg,
+    )}`,
+  );
 }
 
 export function logError(msg: string) {
   console.log(error(msg));
-
 }
 
 export function newLine() {
   console.log();
 }
-
