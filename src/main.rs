71574c2bc2fec440c95e6b1e26e26873536f89aa
gaetbout <gaetan.ansotte@hotmail.com>
2023-08-20 13:34:22 +0200

    reading env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use dotenv::dotenv;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let mailcoach_api_token = std::env::var("COINCAP_API_KEY").expect("COINCAP_API_KEY must be set.");
+
+
+    println!("Somethng {mailcoach_api_token}");
 }
