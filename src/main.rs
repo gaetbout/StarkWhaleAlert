f276210a400905f67f9a03052e0ee188af2ae378
gaetbout <gaetan.ansotte@hotmail.com>
2023-08-20 13:52:36 +0200

    simple get from coincap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 use dotenv::dotenv;
+use reqwest;
+use reqwest::header;
+use std::error::Error;
+use std::time::Duration;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
-    let mailcoach_api_token = std::env::var("COINCAP_API_KEY").expect("COINCAP_API_KEY must be set.");
-
-
+    let mailcoach_api_token =
+        std::env::var("COINCAP_API_KEY").expect("COINCAP_API_KEY must be set.");
     println!("Somethng {mailcoach_api_token}");
+
+    let mut h = header::HeaderMap::new();
+    h.insert(
+        "Content-Type",
+        header::HeaderValue::from_static("application/json"),
+    );
+    h.insert(
+        "Authorization",
+        header::HeaderValue::from_static("Bearer ${coincapApiKey}"),
+    );
+
+    let client = reqwest::Client::builder().default_headers(h).build()?;
+
+    let get_link: String = format!("{}{}", "https://api.coincap.io/v2/assets/", "ethereum");
+
+    let doge = client
+        .get(get_link)
+        .header("Accept", "text/plain")
+        .timeout(Duration::from_secs(3))
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:}", doge);
+    Ok(())
 }
