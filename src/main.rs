d633ba57aa75604c162346d7c863f4ed714501e9
gaetbout <gaetan.ansotte@hotmail.com>
2023-08-20 14:04:28 +0200

    enhance

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use dotenv::dotenv;
 use reqwest;
 use reqwest::header;
+use reqwest::header::HeaderValue;
 use std::error::Error;
 use std::time::Duration;
 
@@ -11,17 +12,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         std::env::var("COINCAP_API_KEY").expect("COINCAP_API_KEY must be set.");
     println!("Somethng {mailcoach_api_token}");
 
-    let mut h = header::HeaderMap::new();
-    h.insert(
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(
         header::CONTENT_TYPE,
-        header::HeaderValue::from_static("application/json"),
+        HeaderValue::from_static("application/json"),
     );
-    h.insert(
+    headers.insert(
         header::AUTHORIZATION,
-        header::HeaderValue::from_static("Bearer ${coincapApiKey}"),
+        HeaderValue::from_static("Bearer ${coincapApiKey}"),
     );
 
-    let client = reqwest::Client::builder().default_headers(h).build()?;
+    let client = reqwest::Client::builder().default_headers(headers).build()?;
 
     let get_link: String = format!("{}{}", "https://api.coincap.io/v2/assets/", "ethereum");
 
