0c8e406144a567b9534573c76553acadce7736b0
gaetbout <gaetan.ansotte@hotmail.com>
2023-08-28 20:09:49 +0200

    adding 1 test

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,8 @@
 
-  use reqwest::header;
-  use reqwest::header::HeaderValue;
-  use std::time::Duration;
-  pub async fn fetch_coin(coin_id: &str){
+use reqwest::header;
+use reqwest::header::HeaderValue;
+use std::time::Duration;
+pub async fn fetch_coin(coin_id: &str) {
     let mut headers = reqwest::header::HeaderMap::new();
     headers.insert(
         header::CONTENT_TYPE,
@@ -12,21 +12,33 @@
         header::AUTHORIZATION,
         HeaderValue::from_static("Bearer ${coincapApiKey}"),
     );
-  
+
     let client = reqwest::Client::builder()
         .default_headers(headers)
-        .build().expect("Failed building the client");
-  
-    let get_link: String = format!("{}{}", "https://api.coincap.io/v2/assets/",coin_id);
-  
+        .build()
+        .expect("Failed building the client");
+
+    let get_link: String = format!("{}{}", "https://api.coincap.io/v2/assets/", coin_id);
+
     let ethereum = client
         .get(get_link)
         .header("Accept", "text/plain")
         .timeout(Duration::from_secs(3))
         .send()
-        .await.unwrap()
+        .await
+        .unwrap()
         .text()
-        .await.unwrap();
-  
+        .await
+        .unwrap();
+
     println!("{:}", ethereum);
-  }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::fetch_coin;
+    #[tokio::test]
+    async fn it_works() {
+        fetch_coin("ethereum").await;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use dotenv::dotenv;
 use std::error::Error;
+
 mod api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     check_valid_env();
-    
+
     api::fetch_coin("ethereum").await;
 
     Ok(())
 }
 
-fn check_valid_env(){
+fn check_valid_env() {
     dotenv().ok();
     let mailcoach_api_token =
         std::env::var("COINCAP_API_KEY").expect("COINCAP_API_KEY must be set.");
-        println!("Somethng {mailcoach_api_token}");
+    println!("Somethng {mailcoach_api_token}");
 }
+/*
+mod rpc;
+mod starknet;
+
+fn main() -> sc_cli::Result<()> {
+    command::run()
+}
+ */
