ced93b1417d8cb8f552102d32eb87e5432c7377c
gaetbout <gaetan.ansotte@hotmail.com>
2023-08-28 19:44:23 +0200

    split into mod

---
+++ src/api.rs
@@ -0,0 +1,32 @@
+
+  use reqwest::header;
+  use reqwest::header::HeaderValue;
+  use std::time::Duration;
+  pub async fn lama(){
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        HeaderValue::from_static("application/json"),
+    );
+    headers.insert(
+        header::AUTHORIZATION,
+        HeaderValue::from_static("Bearer ${coincapApiKey}"),
+    );
+  
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build().expect("Failed building the client");
+  
+    let get_link: String = format!("{}{}", "https://api.coincap.io/v2/assets/", "ethereum");
+  
+    let ethereum = client
+        .get(get_link)
+        .header("Accept", "text/plain")
+        .timeout(Duration::from_secs(3))
+        .send()
+        .await.unwrap()
+        .text()
+        .await.unwrap();
+  
+    println!("{:}", ethereum);
+  }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
 use dotenv::dotenv;
-use reqwest::header;
-use reqwest::header::HeaderValue;
 use std::error::Error;
-use std::time::Duration;
+mod api;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    check_valid_env();
+    
+    api::lama().await;
+
+    Ok(())
+}
+
+fn check_valid_env(){
     dotenv().ok();
     let mailcoach_api_token =
         std::env::var("COINCAP_API_KEY").expect("COINCAP_API_KEY must be set.");
-    println!("Somethng {mailcoach_api_token}");
-
-    let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(
-        header::CONTENT_TYPE,
-        HeaderValue::from_static("application/json"),
-    );
-    headers.insert(
-        header::AUTHORIZATION,
-        HeaderValue::from_static("Bearer ${coincapApiKey}"),
-    );
-
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()?;
-
-    let get_link: String = format!("{}{}", "https://api.coincap.io/v2/assets/", "ethereum");
-
-    let doge = client
-        .get(get_link)
-        .header("Accept", "text/plain")
-        .timeout(Duration::from_secs(3))
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    println!("{:}", doge);
-    Ok(())
+        println!("Somethng {mailcoach_api_token}");
 }
