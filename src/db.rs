70869a35f1c1180b26fdac46b57964b4fb0682dc
gaetbout <gaetan.ansotte@hotmail.com>
2023-08-31 21:09:21 +0200

    run fmté

--- src/db.rs
+++ src/db.rs
@@ -1,9 +1,7 @@
-
 use std::default::Default;
 
-use koit::{FileDatabase, format::Json};
+use koit::{format::Json, FileDatabase};
 use serde::{Deserialize, Serialize};
-
 
 #[derive(Debug, Default, Deserialize, Serialize, Clone)]
 struct Data {
@@ -11,37 +9,52 @@ struct Data {
     twitter_refresh_token: String,
 }
 
-async fn get_db(path:&str) -> FileDatabase<Data,Json> {
-    FileDatabase::<Data, Json>::load_from_path_or_default(path).await.unwrap()
+async fn get_db(path: &str) -> FileDatabase<Data, Json> {
+    FileDatabase::<Data, Json>::load_from_path_or_default(path)
+        .await
+        .unwrap()
 }
 
-pub async fn get_last_processsed_block(path:&str) -> u128 {
-    get_db(path).await.read(|data| data.to_owned()).await.last_processsed_block
+pub async fn get_last_processsed_block(path: &str) -> u128 {
+    get_db(path)
+        .await
+        .read(|data| data.to_owned())
+        .await
+        .last_processsed_block
 }
 
-pub async fn get_twitter_refresh_token(path:&str) -> String {
-    get_db(path).await.read(|data| data.to_owned()).await.twitter_refresh_token
+pub async fn get_twitter_refresh_token(path: &str) -> String {
+    get_db(path)
+        .await
+        .read(|data| data.to_owned())
+        .await
+        .twitter_refresh_token
 }
 
-pub async fn set_last_processsed_block(path:&str, last_processsed_block: u128) {
+pub async fn set_last_processsed_block(path: &str, last_processsed_block: u128) {
     let db = get_db(path).await;
     db.write(|data| {
         data.last_processsed_block = last_processsed_block;
-    }).await;
+    })
+    .await;
     db.save().await.unwrap();
 }
 
-pub async fn set_twitter_refresh_token(path:&str, twitter_refresh_token:String)  {
+pub async fn set_twitter_refresh_token(path: &str, twitter_refresh_token: String) {
     let db = get_db(path).await;
     db.write(|data| {
         data.twitter_refresh_token = twitter_refresh_token;
-    }).await;
+    })
+    .await;
     db.save().await.unwrap();
 }
 #[cfg(test)]
 mod tests {
 
-    use super::{get_last_processsed_block, get_twitter_refresh_token, set_last_processsed_block, set_twitter_refresh_token};
+    use super::{
+        get_last_processsed_block, get_twitter_refresh_token, set_last_processsed_block,
+        set_twitter_refresh_token,
+    };
 
     #[tokio::test]
     async fn test_db() {
@@ -52,5 +65,4 @@ mod tests {
         println!("number {:?}:", number);
         println!("twitter {:?}:", twitter);
     }
-
 }
